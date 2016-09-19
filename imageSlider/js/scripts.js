2d7778eafb405f467e8720472a8cb70692a183d1
Elton <idarktech@yahoo.com>
2016-09-19 16:51:20 +0800

    s

--- imageSlider/js/scripts.js
+++ imageSlider/js/scripts.js
@@ -342,7 +342,7 @@ var Slider = Config.extend({
      */
     resizeWidget: function(height) {
         JFCustomWidget.requestFrameResize({
-            height: height || $('body').outerHeight(true)
+            height: height || $('.container').height();
         });
     },
 
