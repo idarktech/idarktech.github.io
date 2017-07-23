f760c047c03241ccb76e8ea6a8ee440f683b85b4
Elton <idarktech@yahoo.com>
2017-07-23 14:29:35 +0800

    First step - plugin URI

---
+++ wpUpwork/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Plugin Name: Upwork Job Search
+* Plugin URI: http://idarktech.github.io
+* Description: This widget is for custom job posts
+* Author: Tonix Regs
+* Author URI: http://idarktech.github.io
+* Version: 0.1
+* License: GPLv2
+*/
+
+//Exit if accessed directly
+
+if (!defined('ABSPATH')){
+	exit;
+}
+
+
+?>
