4ff9f6a75988fd448d9eb50a4c835d25af242e79
Elton <idarktech@yahoo.com>
2017-07-24 11:30:55 +0800

    changes

--- daycountdown/assets/js/script.js
+++ daycountdown/assets/js/script.js
@@ -8,24 +8,17 @@ $(function () {
         {
             var note = $('#note');
 
-            var year = JFCustomWidget.getWidgetSetting('year');
-            var month = JFCustomWidget.getWidgetSetting('month') - 1;
-            var day = JFCustomWidget.getWidgetSetting('day');
             var hours = JFCustomWidget.getWidgetSetting('hour');
             var minutes = JFCustomWidget.getWidgetSetting('minute');
             var seconds = 0;
             var milliseconds = 0;
 
-            $('#countdown').countdown(new Date(year, month, day, hours, minutes, seconds, milliseconds),function (event) {
+            $('#countdown').countdown(new Date(hours, minutes, seconds, milliseconds),function (event) {
 
-                    var nd = event.strftime('%D');
                     var nh = event.strftime('%H');
                     var nm = event.strftime('%M');
                     var ns = event.strftime('%S');
-                    
-                    while(nd.length<3){
-                        nd = "0"+nd;
-                    }
+
                     if($($(".countSeconds .position")[0]).find(".digit").html()[0]!==ns[0]){
                         $($(".countSeconds .position")[0]).find(".digit").animate({top: "50px", opacity: 0.0}, 100)
                             .animate({top: "-50px"}, 200, function(){$(this).html(ns[0]);})
@@ -64,27 +57,8 @@ $(function () {
                             .animate({top: "0px", opacity: 1}, 100)
                             .animate({top: "0px"} , 200);	
                     }
-
-                    if($($(".countDays .position")[0]).find(".digit").html()[0]!==nd[0]){
-                        $($(".countDays .position")[0]).find(".digit").animate({top: "50px", opacity: 0.0}, 100)
-                            .animate({top: "-50px"}, 200, function(){$(this).html(nd[0]);})
-                            .animate({top: "0px", opacity: 1}, 100)
-                            .animate({top: "0px"} , 200);	
-                    } 
-                    if($($(".countDays .position")[1]).find(".digit").html()[0]!==nd[1]){
-                        $($(".countDays .position")[1]).find(".digit").animate({top: "50px", opacity: 0.0}, 100)
-                            .animate({top: "-50px"}, 200, function(){$(this).html(nd[1]);})
-                            .animate({top: "0px", opacity: 1}, 100)
-                            .animate({top: "0px"} , 200);	
-                    }
-                    if($($(".countDays .position")[2]).find(".digit").html()[0]!==nd[2]){
-                        $($(".countDays .position")[2]).find(".digit").animate({top: "50px", opacity: 0.0}, 100)
-                            .animate({top: "-50px"}, 200, function(){$(this).html(nd[2]);})
-                            .animate({top: "0px", opacity: 1}, 100)
-                            .animate({top: "0px"} , 200);	
-                    }
                     
-                    note.html(event.strftime('%D days, %H hours, %M minutes and %S seconds left!'));
+                    note.html(event.strftime('%H hours, %M minutes and %S seconds left!'));
                 
             });
 
